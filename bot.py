56537137f7603c7b67b6852021b7229f25d1c622
Christopher Vu <crystoffar@gmail.com>
2023-05-20 23:26:03 -0700

    added basic sqlite3 functionality

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 
 import discord
 from discord.ext import commands
+import sqlite3
 
 description = 'A bot for helping AMC screenings'
 
@@ -25,7 +26,51 @@ async def on_member_join(member):
         await guild.system_channel.send(to_send)
 
 @bot.command()
+async def trackAdd(interaction: discord.Interaction, member: discord.Member, entries, cd):
+    '''Adds user to tracker'''
+    user = member.id
+    guild = member.guild
+    conn = sqlite3.connect('users.db')
+    cur = conn.cursor()
+    cur.execute('INSERT INTO tracker VALUES(?, ?, ?)', (user, entries, cd))
+    conn.commit()
+    conn.close()
+    await guild.system_channel.send(member.name + " added to tracker")
+
+@bot.command()
+async def check(ctx):
+    '''checks cooldown/entries of user'''
+    user = ctx.message.author.id
+    conn = sqlite3.connect('users.db')
+    cur = conn.cursor()
+    cur.execute('SELECT cooldown FROM tracker WHERE userid = ?', (user,))
+    row = cur.fetchone()
+
+    if (row is not None) and (int(row[0]) != 0):
+        cd = str(row[0])
+        await ctx.send(ctx.message.author.name + " is on cooldown for " + cd + " more screenings.")
+    else:
+        cur.execute('SELECT entries FROM tracker WHERE userid = ?', (user,))
+        row = cur.fetchone()
+        if row is not None:
+            entries = str(row[0])
+            await ctx.send(ctx.message.author.name + " has " + entries + " entries.")
+            cur.execute('SELECT SUM(entries) FROM tracker')
+            totalEntries = cur.fetchone()[0]
+            await ctx.send("You have a " + '{:.1%}'.format(int(entries)/totalEntries) + " chance to win!")
+
+@bot.command()
+async def temp(ctx):
+    print("adding table!")
+    conn = sqlite3.connect('users.db')
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE tracker("userid", "entries", "cooldown")')
+    conn.commit()
+    conn.close()
+
+@bot.command()
 async def speech(ctx):
+    '''Recites Nicole Kidman's speech'''
     with open('speech.txt') as file:
         lines = [line.strip() for line in file]
     speech = ''
