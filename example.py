51b4acef1ec3dcd0b9e0dba5351e7d059edb1454
Christopher Vu <crystoffar@gmail.com>
2023-05-20 17:51:37 -0700

    implemented basic commands

--- example.py
+++ example.py
@@ -1,22 +1,35 @@
 # This example requires the 'message_content' intent.
 
 import discord
+from discord.ext import commands
 
+description = 'A bot for helping AMC screenings'
+
+#sets intents for bot
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', description =description, intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def speech(ctx):
+    with open('speech.txt') as file:
+        lines = [line.strip() for line in file]
+    speech = ''
+    for line in lines:
+        speech += line
+        speech += '\n'
+    await ctx.send(speech)
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+#loads token from token.txt and saves in token string
+with open('token.txt') as file:
+    lines = [line.strip() for line in file]
+token = lines[0]
 
-client.run('your token here')
+bot.run(token)
